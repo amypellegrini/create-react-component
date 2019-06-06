45929e653006c711c2af28c41cb3eacda2b2007c
Amy Pellegrini <amyvpellegrini@gmail.com>
2019-06-06 22:22:35 +0100

    Implement folder generation and POSIX input validation

--- create-react-component/index.js
+++ create-react-component/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const fs = require("fs");
+
+function createReactComponent(name) {
+  if (name.match(/\W/)) {
+    throw new Error(
+      "Component name should be composed of letters, numbers, or hyphens (_, -)"
+    );
+  }
+
+  fs.mkdirSync(name);
+}
+
+module.exports = createReactComponent;
--- test/test.js
+++ test/test.js
@@ -1,18 +1,48 @@
 "use strict";
 
-/**
- * It should accept a `--componentName=` parameter followed by
- * the desired component name.
- *
- * E.g., `create-react-component --componentName=example-component`
- */
-
-const createReactComponent = require("../create-react-component");
+const fs = require("fs");
 const should = require("chai").should();
 const expect = require("chai").expect;
+const createReactComponent = require("../create-react-component");
 
-describe("create-react-component module", function() {
-  it("should be an object", function() {
-    createReactComponent.should.be.an("object");
+describe("createReactComponent module", function() {
+  it("should be a function", function() {
+    createReactComponent.should.be.a("function");
+  });
+});
+
+describe("component folder generation", function() {
+  const componentName = "componentName";
+
+  it("should create a folder matching a given component name string", function() {
+    createReactComponent(componentName);
+
+    const folderExists = fs.existsSync(componentName);
+
+    expect(folderExists).to.be.true;
+  });
+
+  after(function() {
+    fs.rmdirSync(componentName);
+  });
+});
+
+describe("component name input validation", function() {
+  const invalidName = "$£%^&";
+
+  /**
+   * POSIX portable filename charater set:
+   *   https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_282
+   */
+  it("should error incompatible POSIX portable filename", function() {
+    expect(
+      createReactComponent.bind(createReactComponent, invalidName)
+    ).to.throw();
+  });
+
+  after(function() {
+    if (fs.existsSync(invalidName)) {
+      fs.rmdirSync(invalidName);
+    }
   });
 });
