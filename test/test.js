a448d3480eb84f42a62cde9447bbb4bbe2845461
Amy Pellegrini <amyvpellegrini@gmail.com>
2019-06-06 11:16:30 +0100

    Implement initial (failing) test

---
+++ test/test.js
@@ -0,0 +1,16 @@
+/**
+ * It should accept a `--componentName=` parameter followed by
+ * the desired component name.
+ *
+ * E.g., `create-react-component --componentName=example-component`
+ */
+
+const createReactComponent = require("../create-react-component");
+const should = require("chai").should();
+const expect = require("chai").expect;
+
+describe("create-react-component module", function() {
+  it("should be an object", function() {
+    createReactComponent.should.be.an("object");
+  });
+});
